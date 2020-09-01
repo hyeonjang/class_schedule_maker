c36e8e3868c3111606e0e841abae593ac4948b7f
hyeonjang <hyeonjang.an@skku.edu>
2020-09-02 06:56:54 +0900

    little change on attributes

--- timetable/models.py
+++ timetable/models.py
@@ -6,13 +6,13 @@ from school.models import Term, ClassRoom, Subject
 
 # Create your models here.
 class TimeTable(models.Model):
-    teacher   = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    semester  = models.ForeignKey(Term,      on_delete=models.CASCADE, blank=True)
-    classRoom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE, null=True, blank=True)
-    subject   = models.ForeignKey(Subject,   on_delete=models.CASCADE, null=True, blank=True)
-    weekday   = models.DateField(default=timezone.now, null=True, blank=True)
-    time      = models.SmallIntegerField(default=0)
-    created_time = models.DateTimeField(default=timezone.now)
+    teacher   = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # works in view.py
+    semester  = models.ForeignKey(Term,      on_delete=models.CASCADE, blank=True) # works in view.py
+    classRoom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE, null=True, blank=True) # user choice
+    subject   = models.ForeignKey(Subject,   on_delete=models.CASCADE, null=True, blank=True) # user choice
+    weekday   = models.DateField(default=timezone.now, null=True, blank=True) # works in view.py
+    time      = models.SmallIntegerField(default=0, blank=True) # works in view.py
+    created_time = models.DateTimeField(default=timezone.now) # works in view.py
 
     class Meta:
         unique_together = ['weekday', 'time']
