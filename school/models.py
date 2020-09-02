c3c5ea22f7176186a4c8340a7baf234d2d164ad8
hyeonjang <hyeonjang.an@skku.edu>
2020-09-03 03:15:28 +0900

    little migrations

--- school/models.py
+++ school/models.py
@@ -19,13 +19,24 @@ class Term(models.Model):
 
 class ClassRoom(models.Model):
     classGrade  = models.SmallIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(6)])
-    classNumber = models.SmallIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(20)])
+    classNumber = models.SmallIntegerField(default=1, validators=[MinValueValidator(0), MaxValueValidator(20)])
 
     def __str__(self):
         return f'{self.classGrade}-{self.classNumber}'
+    
+    def c_str(self):
+        return f'{self.classGrade}-{self.classNumber}'
+
+    class Meta:
+        unique_together = ('classGrade', 'classNumber')
 
 class Subject(models.Model):
     name = models.CharField(max_length=64)
+    grade = models.SmallIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(6)])
+    lessonNumber = models.IntegerField(default=272, validators=[MinValueValidator(128), MaxValueValidator(448)]) # 2018년도 교육과정 기준
 
     def __str__(self):
-        return self.name    
+        return  f'{self.grade}'+'학년 ' + self.name
+
+    class Meta:
+        unique_together = ('name', 'grade')
