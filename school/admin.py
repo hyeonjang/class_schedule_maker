862c196d93125b79a38eec6533636ec093577b68
hyeonjang <hyeonjang.an@skku.edu>
2020-09-06 00:20:05 +0900

    school migrations

--- school/admin.py
+++ school/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Term, ClassRoom, Subject
+from .models import Term, ClassRoom, Subject, Holiday, School
 
 # Register your models here.
+admin.site.register(School)
+
+admin.site.register(Holiday)
 admin.site.register(Term)
 admin.site.register(ClassRoom)
 admin.site.register(Subject)
+
