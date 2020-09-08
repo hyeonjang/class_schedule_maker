c411e010b1fdd8826b64aff8498a1e63a7502c70
hyeonjang <hyeonjang.an@skku.edu>
2020-09-09 00:53:36 +0900

    subject & homeroom table added

--- timetable/models.py
+++ timetable/models.py
@@ -4,7 +4,7 @@ Abstract TimeTable model
 from django.db import models
 from django.utils import timezone
 
-from accounts.models import User
+from accounts.models import User, SubjectTeacher, HomeTeacher
 from school.models import Term, ClassRoom, Subject
 
 # Create your models here.
@@ -14,18 +14,18 @@ class TimeTable(models.Model):
     '''
     teacher = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     semester = models.ForeignKey(Term, on_delete=models.CASCADE, blank=True) # works in view.py
-    classRoom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE, null=True, blank=True) #user choice
+    classroom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE, null=True, blank=True) #user choice
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True, blank=True) #user choice
     weekday = models.DateField(default=timezone.now, null=True, blank=True) # works in view.py
     time = models.SmallIntegerField(default=0, blank=True) # works in view.py
 
     class Meta:
-        unique_together = ['weekday', 'time', 'classRoom']
+        unique_together = (('weekday', 'time', 'teacher'),)
         abstract = True
 
     def __str__(self):
-        if self.classRoom:
-            result = self.classRoom.c_str()+'반 '+f'{self.time}교시 '
+        if self.classroom:
+            result = self.classroom.c_str()+'반 '+f'{self.time}교시 '
         else:
             result = f'{self.time}교시 '
         if self.subject:
@@ -37,12 +37,14 @@ class SubjectTable(TimeTable):
     """
     for Subject Teacher
     """
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='main_sub_teacher')
+
     def __str__(self):
-        result = super().teacher
-        if self.classRoom:
-            result += self.classRoom.c_str()+'반 '+f'{self.time}교시 '
+        result = super().teacher.to_string() + ', '
+        if self.classroom:
+            result += self.classroom.c_str()+'반, '+f'{self.time}교시 '
         else:
-            result = f'{self.time}교시 '
+            result += f'{self.time}교시 '
         if self.subject:
             result += self.subject.name
         result += f', {self.weekday}'
@@ -52,5 +54,18 @@ class HomeTable(TimeTable):
     """
     for Homeroom Teacher
     """
-    # sub_teacher = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='subject')
-      
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='main_home_teacher')
+    sub_teacher = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='subject_teacher')
+
+    def __str__(self):
+        result = super().teacher.to_string() + ', '
+        if self.classroom:
+            result += self.classroom.c_str()+'반, '+f'{self.time}교시, '
+        else:
+            result += f'{self.time}교시, '
+        if self.subject:
+            result += self.subject.name + ', '
+        if self.sub_teacher:
+            result +=  self.sub_teacher.to_string() + ', '
+        result += f'{self.weekday}'
+        return result
