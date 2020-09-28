d9d3c07e1385a0b4877cebba6fe88e869b6ba6cb
hyeonjang <hyeonjang.an@skku.edu>
2020-09-28 23:54:13 +0900

    date url argu and added invited url

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,10 +1,12 @@
 '''
 modul doc
 '''
-from django.urls import path
-from . import views
+from django.urls import path, register_converter
+from . import converters, views
 
 #url(r'^user/(?P<user_id>\d+)/profile/$', 'yourapp.views.view', name='user_url')
+
+register_converter(converters.DateConverter, 'date')
 
 app_name = 'timetable'
 
@@ -12,7 +14,12 @@ urlpatterns = [
     path('<int:user_id>/sub/view/', views.SubjectView.as_view(), name='sub_view'),
     path('<int:user_id>/sub/create/', views.SubjectCreate.as_view(), name='sub_create'),
     path('<int:user_id>/sub/update/', views.SubjectUpdate.as_view(), name='sub_update'),
-    path('<int:user_id>/home/view/', views.HomeRoomView.as_view(), name='home_view'),
     path('<int:user_id>/home/create/', views.HomeRoomCreate.as_view(), name='home_create'),
-    path('<int:user_id>/home/update/', views.HomeRoomUpdate.as_view(), name='home_update'),
+    path('<int:user_id>/<date:start>/<date:end>/home/view/', views.HomeRoomView.as_view(), name='home_view'),
+    path('<int:user_id>/<date:start>/<date:end>/home/update/', views.HomeRoomUpdate.as_view(), name='home_update'),
+    path('<int:user_id>/inv/create/', views.InvitedCreate.as_view(), name='inv_create'),
+    path('<int:user_id>/<date:start>/<date:end>/inv/view/', views.InvitedView.as_view(), name='inv_view'),
+    path('<int:user_id>/<date:start>/<date:end>/inv/update/', views.InvitedUpdate.as_view(), name='inv_update'),
+
+
 ]
