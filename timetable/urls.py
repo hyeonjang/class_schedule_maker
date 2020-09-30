fe93704ce38ee7d35af1c4af08676a3bacfee358
hyeonjang <hyeonjang.an@skku.edu>
2020-09-30 23:55:38 +0900

    now subject table has whole days in semester

--- timetable/urls.py
+++ timetable/urls.py
@@ -11,9 +11,9 @@ register_converter(converters.DateConverter, 'date')
 app_name = 'timetable'
 
 urlpatterns = [
-    path('<int:user_id>/sub/view/', views.SubjectView.as_view(), name='sub_view'),
     path('<int:user_id>/sub/create/', views.SubjectCreate.as_view(), name='sub_create'),
-    path('<int:user_id>/sub/update/', views.SubjectUpdate.as_view(), name='sub_update'),
+    path('<int:user_id>/<date:start>/<date:end>/sub/view/', views.SubjectView.as_view(), name='sub_view'),
+    path('<int:user_id>/<date:start>/<date:end>/sub/update/', views.SubjectUpdate.as_view(), name='sub_update'),
     path('<int:user_id>/home/create/', views.HomeRoomCreate.as_view(), name='home_create'),
     path('<int:user_id>/<date:start>/<date:end>/home/view/', views.HomeRoomView.as_view(), name='home_view'),
     path('<int:user_id>/<date:start>/<date:end>/home/update/', views.HomeRoomUpdate.as_view(), name='home_update'),
