0a68fe970ae145cf02a5e82754b3bd056477db83
hyeonjang <hyeonjang.an@skku.edu>
2020-10-02 00:16:03 +0900

    moved to view

--- timetable/utils.py
+++ timetable/utils.py
@@ -5,34 +5,7 @@ import school
 import accounts
 from .models import HomeTable
 
-def create_information(user):
-    '''
-    Return TimeTable information to Templates
-    '''
-    information = dict()
 
-    teacher = accounts.models.HomeTeacher.objects.get(user=user)
-    semester = school.models.Term.get_current()
-    subjects = school.models.Subject.objects.filter(grade=teacher.get_grade())
-
-    for subject in subjects:
-        weeks = semester.get_weeks_start_end()
-        count_per_week = []
-
-        for week in weeks:
-            start = week[0].strftime("%Y-%m-%d")
-            end = week[1].strftime("%Y-%m-%d")
-            count_per_week.append(
-                HomeTable.objects.filter(teacher=user, subject=subject, day__range=(start, end)).count()
-                )
-        dic = {subject:[
-            count_per_week,
-            HomeTable.objects.filter(teacher=user, subject=subject).count(),
-            subject.count
-            ]}
-
-        information.update(dic)
-    return information
 
 def create_list_for_weeks():
     semester = school.models.Term.get_current()
