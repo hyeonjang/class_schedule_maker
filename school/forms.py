b8fbcbe6befeba733b83e77c7ed18c40a7053ae4
hyeonjang <hyeonjang.an@skku.edu>
2020-10-06 02:10:35 +0900

    cleaned up

--- school/forms.py
+++ school/forms.py
@@ -2,25 +2,9 @@
 School Information Form
 '''
 from django import forms
-from django.core.exceptions import ValidationError
 from bootstrap_modal_forms.forms import BSModalModelForm, BSModalForm
 from accounts.models import User
 from .models import Term, Holiday, ClassRoom, Subject
-
-class TimeTableCreation(forms.Form):
-    semester = forms.ModelChoiceField(Term.objects.all())
-    classroom = forms.ModelChoiceField(ClassRoom.objects.all(), required=False)
-    teacher = forms.ModelChoiceField(User.objects.all(), required=False)
-
-    def clean(self):
-        classroom = self.cleaned_data.get('classroom')
-        teacher = self.cleaned_data.get('teacher')
-
-        if classroom and teacher:
-            raise ValidationError('please select classroom or subject')
-
-        if classroom is None and teacher is None:
-            raise ValidationError('please select classroom or subject')
 
 class TermModelForm(BSModalModelForm):
     class Meta:
