36c14049c8260ec15e3be2624ea43f09696c5125
hyeonjang <hyeonjang.an@skku.edu>
2020-10-08 03:43:34 +0900

    removed function

--- timetable/utils.py
+++ timetable/utils.py
@@ -13,23 +13,3 @@ def create_list_for_weeks():
     for days in weeks:
         week_list.append((days[0], days[1]))
     return week_list
-
-def create_homeroom_timetable(home, semester):
-    '''
-    instantiating timetable when creating homeroom teacher
-    '''
-    mon_to_fri = semester.get_week()
-    weeks = semester.get_count_of_weeks()
-
-    bulk_tables = []
-    for i in range(0, weeks):
-        for j in range(0, 40):
-            new_table = timetable.models.HomeTable(
-                teacher=accounts.models.User.get_from(teacher=home),
-                semester=semester,
-                day=mon_to_fri[j%5] + timezone.timedelta(days=7*i),
-                time=(j//5)%8+1,
-                )
-            bulk_tables.append(new_table)
-
-    timetable.models.HomeTable.objects.bulk_create(bulk_tables)
