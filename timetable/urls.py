6c3a8c78eae00cd228988bdc9d5d4bb436656713
hyeonjang <hyeonjang.an@skku.edu>
2020-10-08 03:43:15 +0900

    syntax little change

--- timetable/urls.py
+++ timetable/urls.py
@@ -15,12 +15,12 @@ urlpatterns = [
     path('<int:user_id>/sub/reset/', views.SubjectReset.as_view(), name='sub_reset'),
     path('<int:user_id>/<date:start>/<date:end>/sub/view/', views.SubjectView.as_view(), name='sub_view'),
     path('<int:user_id>/<date:start>/<date:end>/sub/update/', views.SubjectUpdate.as_view(), name='sub_update'),
-    
-    path('<int:user_id>/home/create/', views.HomeRoomCreate.as_view(), name='home_create'),
-    path('<int:user_id>/home/reset/', views.HomeRoomReset.as_view(), name='home_reset'),
-    path('<int:user_id>/<date:start>/<date:end>/home/view/', views.HomeRoomView.as_view(), name='home_view'),
-    path('<int:user_id>/<date:start>/<date:end>/home/update/', views.HomeRoomUpdate.as_view(), name='home_update'),
-    
+
+    path('<int:user_id>/home/create/', views.HomeroomCreate.as_view(), name='home_create'),
+    path('<int:user_id>/home/reset/', views.HomeroomReset.as_view(), name='home_reset'),
+    path('<int:user_id>/<date:start>/<date:end>/home/view/', views.HomeroomView.as_view(), name='home_view'),
+    path('<int:user_id>/<date:start>/<date:end>/home/update/', views.HomeroomUpdate.as_view(), name='home_update'),
+
     path('<int:user_id>/inv/create/', views.InvitedCreate.as_view(), name='inv_create'),
     path('<int:user_id>/sub/reset/', views.InvitedReset.as_view(), name='inv_reset'),
     path('<int:user_id>/<date:start>/<date:end>/inv/view/', views.InvitedView.as_view(), name='inv_view'),
